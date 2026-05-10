ebaab143d9a25047fcb8c417149936c3c5c849a2
Aldiyor <abdullaevaldiyor@gmail.com>
2026-05-10 19:23:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/api/book', require('./api/book'));
+app.get('/api/reservation/:ref', require('./api/getReservation'));
+app.post('/api/cancel', require('./api/cancel'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
